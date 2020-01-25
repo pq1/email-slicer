3591fc869691047b746d3a4d24a9c965dc87d389
PQ1 <pq1@users.noreply.github.com>
2020-01-24 20:24:36 -0600

    updated with various data types

--- email.py
+++ email.py
@@ -1,7 +1,18 @@
-email = input('Enter Email: ')
+# Read items from a list. Accounting for all data types if possible
+x = ['goku@dbz.com', 2, 3.14, 'vegeta@super.com', 'pquach', True]
 
-if '@' in email:
-  email_slicer = email.split('@')
-  print(email_slicer)
-else:
-  print('Enter in a correct email')
+def slice(alist):
+  user_name = []
+  domain_name = []  
+  for x in alist:
+    try:
+      if '@' in x:
+        email_slicer = x.split('@')
+        #print(email_slicer)
+        user_name.append(email_slicer[0])
+        domain_name.append(email_slicer[1])
+    except TypeError:
+      continue
+  return 'Usernames: {0} \nDomains: {1}'.format(user_name, domain_name)
+
+print(slice(x))

b8dbf11f6aae41d1829dd07339e45cdc3e8d5e31
PQ1 <philipquach@outlook.com>
2020-01-25 09:54:51 +0000

    edit list

--- email.py
+++ email.py
@@ -1,5 +1,5 @@
 # Read items from a list. Accounting for all data types if possible
-x = ['goku@dbz.com', 2, 3.14, 'vegeta@super.com', 'pquach', True]
+x = ['goku@dbz.com', 2, 3.14, 'vegeta@super.com', 'pq1', True]
 
 def slice(alist):
   user_name = []
@@ -8,7 +8,6 @@ def slice(alist):
     try:
       if '@' in x:
         email_slicer = x.split('@')
-        #print(email_slicer)
         user_name.append(email_slicer[0])
         domain_name.append(email_slicer[1])
     except TypeError:

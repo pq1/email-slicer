4df54d75cb3aede28c597d3aa785460c283c64cd
PQ1 <philipquach@outlook.com>
2020-01-25 10:04:19 +0000

    added main function

--- email.py
+++ email.py
@@ -1,7 +1,7 @@
 # Read items from a list. Accounting for all data types if possible
 x = ['goku@dbz.com', 2, 3.14, 'vegeta@super.com', 'pq1', True]
 
-def slice(alist):
+def main(alist):
   user_name = []
   domain_name = []  
   for x in alist:
@@ -14,4 +14,5 @@ def slice(alist):
       continue
   return 'Usernames: {0} \nDomains: {1}'.format(user_name, domain_name)
 
-print(slice(x))
+if __name__ == '__main__':
+    print(main(x))
